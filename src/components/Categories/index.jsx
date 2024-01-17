377d9a7e31e677f271b4559bc765fbf4d4fda575
Fahmi <itsfahmiefendy@gmail.com>
2024-01-17 11:17:23 +0700

    fix: get categories from themealdb

--- src/components/Categories/index.jsx
+++ src/components/Categories/index.jsx
@@ -1,43 +1,59 @@
-import { useState } from "react";
 import { Box, Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
 
 import classes from "./style.module.scss";
+import { callApi } from "../../domain/api";
 
 const Categories = () => {
-  const categories = [
-    "Beef",
-    "Chicken",
-    "Dessert",
-    "Lamb",
-    "Miscellaneous",
-    "Pasta",
-    "Favorite",
-  ];
-
+  const [categoryList, setCategoryList] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState("Beef");
 
   const changeCategoryHandler = (c) => {
     setSelectedCategory(c);
   };
 
+  useEffect(() => {
+    const FetchData = async () => {
+      const response = await callApi("/categories.php", "GET");
+
+      const slicedResponse = response.categories.slice(0, 6);
+
+      const formattedCategory = [
+        ...slicedResponse,
+        {
+          idCategory: 7,
+          strCategory: "Favorite",
+        },
+      ];
+
+      setCategoryList(formattedCategory);
+    };
+
+    FetchData();
+  }, []);
+
   return (
-    <Box className={classes.category}>
-      {categories.map((c, i) => {
-        return (
-          <Typography
-            key={i}
-            className={
-              selectedCategory === c
-                ? classes.category__list_active
-                : classes.category__list
-            }
-            onClick={() => changeCategoryHandler(c)}
-          >
-            {c}
-          </Typography>
-        );
-      })}
-    </Box>
+    <React.Fragment>
+      {categoryList.length > 0 && (
+        <Box className={classes.category}>
+          {categoryList.map((c) => {
+            return (
+              <Typography
+                key={c.idCategory}
+                className={
+                  selectedCategory === c
+                    ? classes.category__list_active
+                    : classes.category__list
+                }
+                onClick={() => changeCategoryHandler(c)}
+              >
+                {c.strCategory}
+              </Typography>
+            );
+          })}
+        </Box>
+      )}
+    </React.Fragment>
   );
 };
 
